dd348e97d11c4bcf046746fa3fed3651a71c6421
GMG-Executive <randf77@gmail.com>
2017-09-07 11:43:53 +0700

    Finish Up Options and Change wrong Title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gentelella Alela! | </title>
+    <title>Lowids | Admin</title>
 
     <!-- Bootstrap -->
     <link href="{{ URL::asset('css/bootstrap.css') }}" rel="stylesheet">
