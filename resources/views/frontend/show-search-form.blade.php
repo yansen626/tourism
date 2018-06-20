53df0e7ed00ddd62056bfa2414ef87c7de3daf3d
ferdyantorand <randf77@gmail.com>
2018-06-20 19:49:12 +0700

    Added Datetimepicker.css and js added moment.js Fixed View for Submit Make your own Trip

--- resources/views/frontend/show-search-form.blade.php
+++ resources/views/frontend/show-search-form.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.frontend')
+@extends('layouts.frontend_2')
 
 @section('body-content')
     <!-- content-->
@@ -15,31 +15,30 @@
                                         <div class="billing-wrapper">
                                             <h2 class="title-section mb-5"><span style="color:#35A6DC;">MAKE YOUR</span><br> OWN TRIP</h2>
                                             <div class="woocommerce-billing-fields">
-                                                <p id="billing_first_name_field" class="form-row form-row-first validate-required">
-                                                    <input id="billing_first_name" type="text" name="billing_first_name" placeholder="USERNAME" value="" class="input-text">
-                                                </p>
-                                                <p id="billing_last_name_field" class="form-row form-row-last validate-required">
-                                                    <input id="billing_last_name" type="text" name="billing_last_name" placeholder="EMAIL" value="" class="input-text">
+                                                <p id="billing_first_name_field" class="form-row form-row-wide validate-required">
+                                                    <input id="email" type="text" name="email" placeholder="EMAIL"/>
                                                 </p>
                                                 <div class="clear"></div>
-                                                <p id="billing_company_field" class="form-row form-row-wide">
-                                                    <input placeholder="Depart date" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" class="calendar-default textbox-n">
+                                                <p id="billing_first_name_field" class="form-row form-row-first validate-required">
+                                                    <input id="start" type="text" name="start" placeholder="START DATE" />
                                                 </p>
                                                 <p id="billing_first_name_field" class="form-row form-row-first validate-required">
-                                                    <input id="billing_first_name" type="text" name="billing_first_name" placeholder="DESTINATION" value="" class="input-text">
+                                                    <input id="finish" type="text" name="finish" placeholder="FINISH DATE"/>
+                                                </p>
+
+                                                <p id="billing_first_name_field" class="form-row form-row-first validate-required">
+                                                    <input id="destination" type="text" name="destination" placeholder="DESTINATION"/>
                                                 </p>
                                                 <p id="billing_last_name_field" class="form-row form-row-last validate-required">
-                                                    <input id="billing_last_name" type="text" name="billing_last_name" placeholder="PARTICIPANT" value="" class="input-text">
+                                                    <input id="participant" type="text" name="participant" placeholder="PARTICIPANT" />
                                                 </p>
+
                                                 <p class="form-row form-row-wide address-field">
-                                                    <input id="billing_address_2" type="text" name="billing_address_2" placeholder="TYPE OF TRANSPORT" value="" class="input-text">
-                                                </p>
-                                                <p class="form-row form-row-wide address-field">
-                                                    <input id="billing_address_2" type="text" name="billing_address_2" placeholder="BUDGET" value="" class="input-text">
+                                                    <input id="budget" type="text" name="budget" placeholder="BUDGET PER PERSON" />
                                                 </p>
                                                 <div class="clear"></div>
                                                 <p id="order_comments_field" class="form-row notes mt-20 mb-20">
-                                                    <textarea id="order_comments" name="order_comments" placeholder="REQUEST" rows="2" cols="5" class="input-text"></textarea>
+                                                    <textarea id="order_comments" name="order_comments" placeholder="REQUEST" rows="2" cols="6" class="input-text"></textarea>
                                                 </p>
                                             </div>
                                         </div>
@@ -131,8 +130,89 @@
             </div>
         </div>
     </div>
-    <!-- ! content-->
 
 
 	@include('frontend.partials._modal-login')
+@endsection
+
+@section('styles')
+    @parent
+    <link rel="stylesheet" href="{{ URL::asset('css/frontend/bootstrap-datetimepicker.css') }}">
+    <style>
+
+        #email{
+            border-radius: 30px; border-color: rgb(201,225,230); border-width: 3px;
+            background-image: url('{{ URL::asset('frontend_images/envelope.png') }}');
+            -webkit-background-size: 15px 15px; background-size: 15px 15px;
+            background-position: 459px 10px;
+            background-repeat: no-repeat;
+        }
+
+        #destination{
+            border-radius: 30px; border-color: rgb(201,225,230); border-width: 3px;
+            background-image: url('{{ URL::asset('frontend_images/map-marker.png') }}');
+            -webkit-background-size: 15px 15px; background-size: 15px 15px;
+            background-position: 210px 10px;
+            background-repeat: no-repeat;
+        }
+
+        #participant{
+            border-radius: 30px; border-color: rgb(201,225,230); border-width: 3px;
+            background-image: url('{{ URL::asset('frontend_images/multiple-users-silhouette.png') }}');
+            -webkit-background-size: 15px 15px; background-size: 15px 15px;
+            background-position: 210px 10px;
+            background-repeat: no-repeat;
+        }
+
+        #budget{
+            border-radius: 30px; border-color: rgb(201,225,230); border-width: 3px;
+            background-image: url('{{ URL::asset('frontend_images/calculator.png') }}');
+            -webkit-background-size: 15px 15px; background-size: 15px 15px;
+            background-position: 459px 10px;
+            background-repeat: no-repeat;
+        }
+
+        #start{
+            border-radius: 30px; border-color: rgb(201,225,230); border-width: 3px;
+            background-image: url('{{ URL::asset('frontend_images/calendar-with-spring-binder-and-date-blocks.png') }}');
+            -webkit-background-size: 15px 15px; background-size: 15px 15px;
+            background-position: 210px 10px;
+            background-repeat: no-repeat;
+        }
+
+        #finish{
+            border-radius: 30px; border-color: rgb(201,225,230); border-width: 3px;
+            background-image: url('{{ URL::asset('frontend_images/calendar-with-spring-binder-and-date-blocks.png') }}');
+            -webkit-background-size: 15px 15px; background-size: 15px 15px;
+            background-position: 210px 10px;
+            background-repeat: no-repeat;
+        }
+
+        textarea {
+            -webkit-border-radius: 50px;
+            -moz-border-radius: 50px;
+            border-radius: 30px;
+            border-color: rgb(201,225,230);
+            border-width: 3px;
+            resize: none;
+        }
+    </style>
+@endsection
+
+@section('scripts')
+    @parent
+    <script src="{{ URL::asset('js/frontend/moment.js') }}"></script>
+    <script src="{{ URL::asset('js/frontend/bootstrap-datetimepicker.min.js') }}"></script>
+
+    <script type="text/javascript">
+        $(function () {
+            $('#start').datetimepicker({
+                format: "DD MMM Y"
+            });
+
+            $('#finish').datetimepicker({
+                format: "DD MMM Y"
+            });
+        });
+    </script>
 @endsection
