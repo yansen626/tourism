bb5da9a72647cad4cb299eff6bddcc980d5ee518
GMG-Developer <bayuindra091191@gmail.com>
2018-06-29 16:44:57 +0700

    Create package in progress

---
+++ resources/views/frontend/travelmate/packages/create.blade.php
@@ -0,0 +1,348 @@
+@extends('layouts.frontend_2')
+
+@section('body-content')
+    <!-- content-->
+    <div class="content-body">
+        {{--<div class="container page">--}}
+        <div style="margin-top:3%;">
+            <div class="row">
+                @include('frontend.travelmate.partials._left-side')
+                <div class="col-md-7">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h1>MY PACKAGES</h1>
+                        </div>
+                        <div class="col-md-6">
+                        </div>
+                    </div>
+                    <div class="row" style="margin-top: 5px !important;">
+                        <div class="col-md-12">
+
+                            <div class="board">
+                                <!-- <h2>Welcome to IGHALO!<sup>™</sup></h2>-->
+                                <div class="board-inner">
+                                    <ul class="nav nav-tabs" id="myTab">
+                                        <div class="liner"></div>
+                                        <li class="active">
+                                            <a href="#home" data-toggle="tab" title="welcome">
+                                              <span class="round-tabs one">
+                                                      1
+                                              </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#profile" data-toggle="tab" title="profile">
+                                                 <span class="round-tabs two">
+                                                     2
+                                                 </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#doner" data-toggle="tab" title="completed">
+                                                 <span class="round-tabs five">
+                                                      3
+                                                 </span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="tab-content">
+                                    <div class="tab-pane fade in active" id="home">
+                                        <div class="col-lg-12 col-md-12">
+                                            <div class="row form-panel">
+                                                <<div class="col-lg-6 col-md-6">
+                                                    Column 1
+                                                </div>
+                                                <div class="col-lg-6 col-md-6">
+                                                    Column 2
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="profile">
+                                        <h3 class="head text-center">Create a Bootsnipp<sup>™</sup> Profile</h3>
+                                        <p class="narrow text-center">
+                                            Lorem ipsum dolor sit amet, his ea mollis fabellas principes. Quo mazim facilis tincidunt ut, utinam saperet facilisi an vim.
+                                        </p>
+
+                                        <p class="text-center">
+                                            <a href="" class="btn btn-success btn-outline-rounded green"> create your profile <span style="margin-left:10px;" class="glyphicon glyphicon-send"></span></a>
+                                        </p>
+
+                                    </div>
+                                    <div class="tab-pane fade" id="doner">
+                                        <div class="text-center">
+                                            <i class="img-intro icon-checkmark-circle"></i>
+                                        </div>
+                                        <h3 class="head text-center">thanks for staying tuned! <span style="color:#f48260;">♥</span> Bootstrap</h3>
+                                        <p class="narrow text-center">
+                                            Lorem ipsum dolor sit amet, his ea mollis fabellas principes. Quo mazim facilis tincidunt ut, utinam saperet facilisi an vim.
+                                        </p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    @include('frontend.travelmate.partials._right-side')
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ! content-->
+
+    @include('frontend.partials._modal-login')
+@endsection
+
+@section('styles')
+    @parent
+    <link rel="stylesheet" href="{{ URL::asset('css/datatable/jquery.dataTables.min.css') }}">
+    <style>
+        @import url(http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700);
+
+        .board ul li{
+            padding-left: 0 !important;;
+            margin-left: 0 !important;
+        }
+
+        /* written by riliwan balogun http://www.facebook.com/riliwan.rabo*/
+        .board{
+            width: 80%;
+            margin: 60px auto;
+            height: 500px;
+            background: #fff;
+            /*box-shadow: 10px 10px #ccc,-10px 20px #ddd;*/
+        }
+        .board .nav-tabs {
+            position: relative;
+            /* border-bottom: 0; */
+            /* width: 80%; */
+            margin: 40px auto;
+            margin-bottom: 0;
+            box-sizing: border-box;
+
+        }
+
+        .board > div.board-inner{
+            /*background: #fafafa url(http://subtlepatterns.com/patterns/geometry2.png);*/
+            /*background-size: 30%;*/
+        }
+
+        p.narrow{
+            width: 60%;
+            margin: 10px auto;
+        }
+
+        .liner{
+            height: 4px;
+            background: #EB5532;
+            position: absolute;
+            width: 72%;
+            margin: 0 auto;
+            left: 0;
+            right: 0;
+            top: 50%;
+            z-index: 1;
+        }
+
+        .nav-tabs > li.active > a, .nav-tabs > li.active > a:hover, .nav-tabs > li.active > a:focus {
+            color: #555555;
+            cursor: default;
+            /* background-color: #ffffff; */
+            border: 0;
+            border-bottom-color: transparent;
+        }
+
+        span.round-tabs{
+            width: 70px;
+            height: 70px;
+            line-height: 70px;
+            display: inline-block;
+            border-radius: 100px;
+            z-index: 2;
+            position: absolute;
+            left: 0;
+            text-align: center;
+            font-size: 25px;
+            background-color: #EB5532 !important;
+            color: #ffffff !important;
+            border: none !important;
+        }
+
+        span.round-tabs.one{
+            color: rgb(34, 194, 34);border: 2px solid rgb(34, 194, 34);
+        }
+
+        li.active span.round-tabs.one{
+            border: 2px solid #ddd;
+            color: rgb(34, 194, 34);
+        }
+
+        span.round-tabs.two{
+            color: #febe29;border: 2px solid #febe29;
+        }
+
+        li.active span.round-tabs.two{
+            border: 2px solid #ddd;
+            color: #febe29;
+        }
+
+        span.round-tabs.three{
+            color: #3e5e9a;border: 2px solid #3e5e9a;
+        }
+
+        li.active span.round-tabs.three{
+            border: 2px solid #ddd;
+            color: #3e5e9a;
+        }
+
+        span.round-tabs.four{
+            color: #f1685e;border: 2px solid #f1685e;
+        }
+
+        li.active span.round-tabs.four{
+            background: #fff !important;
+            border: 2px solid #ddd;
+            color: #f1685e;
+        }
+
+        span.round-tabs.five{
+            color: #999;border: 2px solid #999;
+        }
+
+        /*li.active span.round-tabs.five{*/
+            /*background: #fff !important;*/
+            /*border: 2px solid #ddd;*/
+            /*color: #999;*/
+        /*}*/
+
+        .nav-tabs > li.active > a span.round-tabs{
+            background: #fafafa;
+        }
+        .nav-tabs > li {
+            width: 33%;
+        }
+        /*li.active:before {
+            content: " ";
+            position: absolute;
+            left: 45%;
+            opacity:0;
+            margin: 0 auto;
+            bottom: -2px;
+            border: 10px solid transparent;
+            border-bottom-color: #fff;
+            z-index: 1;
+            transition:0.2s ease-in-out;
+        }*/
+        li:after {
+            content: " ";
+            position: absolute;
+            left: 45%;
+            opacity:0;
+            margin: 0 auto;
+            bottom: 0px;
+            border: 5px solid transparent;
+            border-bottom-color: #ddd;
+            transition:0.1s ease-in-out;
+
+        }
+        li.active:after {
+            content: " ";
+            position: absolute;
+            left: 45%;
+            opacity:1;
+            margin: 0 auto;
+            bottom: 0px;
+            border: 10px solid transparent;
+            border-bottom-color: #ddd;
+
+        }
+        .nav-tabs > li a{
+            width: 70px;
+            height: 70px;
+            margin: 20px auto;
+            border-radius: 100%;
+            padding: 0;
+        }
+
+        .nav-tabs > li a:hover{
+            background: transparent;
+        }
+
+        .tab-content{
+        }
+        .tab-pane{
+            position: relative;
+            padding-top: 50px;
+        }
+        .tab-content .head{
+            font-family: 'Roboto Condensed', sans-serif;
+            font-size: 25px;
+            text-transform: uppercase;
+            padding-bottom: 10px;
+        }
+        .btn-outline-rounded{
+            padding: 10px 40px;
+            margin: 20px 0;
+            border: 2px solid transparent;
+            border-radius: 25px;
+        }
+
+        .btn.green{
+            background-color:#5cb85c;
+            /*border: 2px solid #5cb85c;*/
+            color: #ffffff;
+        }
+
+        @media( max-width : 585px ){
+
+            .board {
+                width: 90%;
+                height:auto !important;
+            }
+            span.round-tabs {
+                font-size:16px;
+                width: 50px;
+                height: 50px;
+                line-height: 50px;
+            }
+            .tab-content .head{
+                font-size:20px;
+            }
+            .nav-tabs > li a {
+                width: 50px;
+                height: 50px;
+                line-height:50px;
+            }
+
+            li.active:after {
+                content: " ";
+                position: absolute;
+                left: 35%;
+            }
+
+            .btn-outline-rounded {
+                padding:12px 20px;
+            }
+        }
+
+        .form-panel{
+            border: 2px solid #EB5532;
+            border-radius: 4px;
+        }
+    </style>
+@endsection
+
+@section('scripts')
+    @parent
+    <script src="{{ URL::asset('js/frontend/datatable/jquery.dataTables.min.js') }}"></script>
+    <script>
+        $(function(){
+            $('a[title]').tooltip();
+        });
+
+    </script>
+@endsection
